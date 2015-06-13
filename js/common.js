c4bc3fb80130697863ebee230d4ecd05c3221859
huangyunbo <164927900@qq.com>
2015-06-13 20:24:12 +0800

    判断ie6,7,8不动左侧高度。操作栏select统一下样式。左侧菜单zindex层级比个人信息高。在移动端的表格样式变化下

--- js/common.js
+++ js/common.js
@@ -2,6 +2,23 @@
 
 
 $(function(){
+	var browser = (function(){
+		var isIE6 = /msie 6/i.test(navigator.userAgent),
+		isIE7 = /msie 7/i.test(navigator.userAgent),
+		isIE8 = /msie 8/i.test(navigator.userAgent),
+		isIE = /msie/i.test(navigator.userAgent);
+		return {
+			msie:isIE,
+			version:function(){
+				switch(true){
+					case isIE6:return 6;
+					case isIE7:return 7;
+					case isIE8:return 8;
+				}
+			}()
+		};
+	})();
+	
 	function calcSidebar(){//用来控制登陆的布局始终在最左下角
 		var $sidebar = $("#sidebar"),
 		$main = $("#main"),
@@ -10,7 +27,7 @@ $(function(){
 		docW = $(document).width(),
 		winH = $(window).height();
 		
-		if(docW <= 640){
+		if(docW <= 640 && browser.version != 6 && browser.version != 7 && browser.version != 8){
 			$sidebar.height("auto");
 			return;
 		}
