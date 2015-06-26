b91983f5eb1717e74659620986a1bc3ee86ad0c7
huangyunbo <164927900@qq.com>
2015-06-26 19:08:44 +0800

    操作提示js初始化

--- js/common.js
+++ js/common.js
@@ -19,7 +19,7 @@ $(function(){
 		};
 	})();
 	
-	function calcSidebar(){//用来用户名布局默认在最左下角，小于640在右上角
+	function calcSidebar(){//用户名布局默认在最左下角，小于640在右上角
 		var $sidebar = $("#sidebar"),
 		$main = $("#main"),
 		sidebarH = $sidebar.height(),
@@ -46,6 +46,99 @@ $(function(){
 	
 	$("#toggler_menu").click(function(){
 		$("#sidebar_menu").toggle("slow");
-	});
-	
+	});	
 });
+
+//操作提示(1成功/2失败/3加载)
+var Tips = function(){
+	this.option = {type:1,html:""};
+	this.timer;
+	this.init();
+};
+Tips.prototype = {
+	constructor: Tips,
+	ismsgbox: function(){
+		if($("#msgboxdiv").length == 1){
+			if(this.option.type != 1){
+				clearTimeout(this.timer);
+			}
+			this.hidemsgbox();
+		}
+	},
+	hidemsgbox: function(){
+		$("#msgboxdiv").remove();
+	},
+	timedCount: function(){
+		var that = this;
+		clearTimeout(this.timer);
+		this.timer = setTimeout(function(){
+			that.hidemsgbox();
+		}, 5000);
+	},
+	printhtml: function(){
+		var option = arguments[0];
+		var msgboxdiv = document.createElement('div');
+		msgboxdiv.className = 'msgboxdiv';
+		msgboxdiv.id = 'msgboxdiv';
+			
+		if(this.option.type == 1){
+			var msg = document.createElement('span');
+			msg.className = 'msg';
+			msg.innerHTML = '操作成功';
+			if(typeof this.option.html == "string"){
+				msg.innerHTML = this.option.html;
+			}
+			
+			msgboxdiv.appendChild(msg);
+			this.timedCount();
+			
+		}else if(this.option.type == 2){
+			var errmsg = document.createElement('span');
+			errmsg.className = 'errmsg';
+			errmsg.innerHTML = '<div>操作失败</div>';
+			var btn_close_errmsg = document.createElement('i');
+			btn_close_errmsg.className = 'close';
+			btn_close_errmsg.id = 'btn_close_errmsg';
+			btn_close_errmsg.innerHTML = '[关闭]';
+			errmsg.appendChild(btn_close_errmsg);
+			if(typeof this.option.html == "string"){
+				var tempdiv = document.createElement('tempdiv');
+				tempdiv.innerHTML = this.option.html;
+				
+				console.log(errmsg);
+				errmsg.appendChild(tempdiv.childNodes.item(0));
+				errmsg.appendChild(tempdiv.childNodes.item(1));
+				console.log(errmsg);
+				//for(var i=0; i<tempdiv.childNodes.length; i++){
+					//console.log(i);
+					//errmsg.appendChild(tempdiv.childNodes[i]);
+				//}
+				
+			}
+			
+			msgboxdiv.appendChild(errmsg);
+		}
+		
+		this.ismsgbox(); 
+		$("body").append(msgboxdiv);
+	},
+	errmsg: function(){
+		this.option.type = 2;
+		this.option.html = arguments[0];
+		this.printhtml();
+	},
+	msg: function(){
+		this.option.type = 1;
+		this.option.html = arguments[0];
+		this.printhtml();
+	},
+	events: function(){
+		var that = this;
+		$("body").on("click", "#btn_close_errmsg", function(){
+			that.hidemsgbox();
+		});
+	},
+	init: function(){
+		this.events();
+	}
+};
